8d7a46a4a45843487bff1a2ab73b90651ad0faba
Darren Huang <dh475.cs.washington.edu>
2021-03-28 09:25:05 -0700

    Working version v2, not much change

--- index.js
+++ index.js
@@ -2,29 +2,30 @@
 
 // Requirements
 const Discord = require('discord.js');
-const axios = require('axios');
 const dotenv = require('dotenv');
 const oauth = require('oauth');
 const express = require('express');
 const Twitter = require('twitter-lite');
 const app = express();
+// var discord = require('./functions');
 
 const client = new Discord.Client();
 dotenv.config();
 
 /* Verified Users
 ---------------------
-Currently not in use */
-const verified_users = [];
-const pending_users = [];
-const MAX_PENDING_STARTUP = 20;
+Currently not in use 
+// const verified_users = [];
+// const pending_users = [];
+// const MAX_PENDING_STARTUP = 20;
+*/
 
 // Setup oauth
 const request_token_url = 'https://api.twitter.com/oauth/request_token';
 const access_token_url = 'https://api.twitter.com/oauth/access_token';
 const authenticate_url = 'https://api.twitter.com/oauth/authenticate?oauth_token=';
 const consumer = new oauth.OAuth(request_token_url, access_token_url, process.env.CONSUMER_PUBLIC,
-	process.env.CONSUMER_SECRET, '1.0A', 'https://blink-crispy-waffle.herokuapp.com/authenticate', 'HMAC-SHA1');
+	process.env.CONSUMER_SECRET, '1.0A', 'http://localhost:8080/authenticate', 'HMAC-SHA1');
 
 // Meta data
 const twitter_meta = {
@@ -45,13 +46,13 @@ app.get('', (req, res) => {
 app.get('/authenticate', (req, res) => {
 	const verifier = req.query.oauth_verifier;
 	twitter_meta.verifier = verifier;
-	
+
 	consumer.getOAuthAccessToken(twitter_meta.request_token, twitter_meta.request_secret, verifier, function (error, oauthAccessToken, oauthAccessTokenSecret, _) {
 		if (error) {
 			console.log('error: ', error);
 		} else {
 			res.status(200).send('Successful verification. All messages that you send in channels that contain this bot will be tweeted under your ' +
-									'verified account.\n You may close this tab');
+				'verified account.\n You may close this tab');
 			twitter_meta.access_token = oauthAccessToken;
 			twitter_meta.twitter_client = new Twitter({
 				subdomain: 'api',
@@ -88,16 +89,16 @@ const discord_meta = {
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.id}!`);
 	console.log('New welcome message!');
-	while(pending_users.length > MAX_PENDING_STARTUP) {
-		pending_users.shift();
-	}
+	// while (pending_users.length > MAX_PENDING_STARTUP) {
+	// 	pending_users.shift();
+	// }
 });
 
 // Message handling
 client.on('message', msg => {
 	if (msg.author.bot) return;
 	// Supports verification and tweeting for a single channel
-	const channel = client.channels.cache.get(discord_meta.channel_id);
+	const channel = msg.channel;
 	const user = msg.author.id;
 
 	if (msg.content === `${command}authorize`) {
@@ -109,9 +110,13 @@ client.on('message', msg => {
 		remove_tweet(); // Deletes most recent tweet. Only works once
 	} else if (authorized_channel(msg)) { // General case: send a tweet
 		const twitter_client = twitter_meta.twitter_client;
-
+		var body = msg.content;
+		// Include any attachments
+		for (const attachment of msg.attachments) {
+			body += ' ' + attachment[1].attachment;
+		}
 		const tweet = twitter_client.post('statuses/update', {
-			status: msg.content, // msg contains all the meta data, we want only the content
+			status: body, // msg contains all the meta data, we want only the content
 		}).then(res => {
 			// console.log('Successful tweet');
 			// console.log('Tweet id:', res.id_str);
@@ -120,15 +125,19 @@ client.on('message', msg => {
 			console.log('error when tweeting:', console.error(e));
 			channel.send('Error when tweeting');
 		});
-	} else {
-		// console.log('Nonsense');
+	} else if (msg.content === `${command}help`) {
+		channel.send(`${command}authorize to verify, or ${command}delete to remove your most recent tweet.\n` +
+		'delete will remove only one tweet, even if called multiple times in succession');
+
 	}
+	// console.log('Nonsense');
+
 	discord_meta.channel_id = msg.channel.id;
 	discord_meta.channel_name = msg.channel_name;
 });
 
 function authorize(msg) {
-	msg.reply(`You said ${msg.content}`);
+	// msg.reply(`You said ${msg.content}`);
 	consumer.getOAuthRequestToken((err, token, token_secret, _) => {
 		if (err) {
 			console.log(err);
@@ -143,6 +152,7 @@ function authorize(msg) {
 
 function remove_tweet() {
 	const twitter_client = twitter_meta.twitter_client;
+	const channel = client.channels.cache.get(discord_meta.channel_id);
 	twitter_client.post(`statuses/destroy/${twitter_meta.last_tweet_id}`)
 		.then(_ => {
 			channel.send('Deleted previous tweet!');
